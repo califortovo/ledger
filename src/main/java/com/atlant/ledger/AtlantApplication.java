a1412c79fad1d96be40230125ca14c5c8dd6afc5
Alexandr Spiridonov <califortovo@gmail.com>
2022-05-15 12:48:16 +0300

    init commit

---
+++ src/main/java/com/atlant/ledger/AtlantApplication.java
@@ -0,0 +1,13 @@
+package com.atlant.ledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtlantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtlantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/atlant/ledger/AtlantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atlant.ledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtlantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
